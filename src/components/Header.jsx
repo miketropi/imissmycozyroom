e315c196701b914d5e242922a5879489cd1c7448
Mike <mike.beplus@gmail.com>
2024-08-15 16:17:51 +0700

    Update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,23 @@
+import { TypeAnimation } from 'react-type-animation';
+
 export default function Header() {
   return <header className="music-cozy-header">
     <div className="header-container">
-      <a className="site-brand" href="/">i miss my cozy room...</a>
+      <a className="site-brand" href="/">
+        <TypeAnimation
+          sequence={[
+            // Same substring at the start will only be typed out once, initially
+            'I miss my cozy room...',
+            1000, 
+            'my cozy space.',
+            1000, 
+          ]}
+          wrapper="span"
+          speed={50}
+          style={{ fontSize: '1.5em', display: 'inline-block' }}
+          repeat={ Infinity }
+        />
+      </a>
       <div className="site-nav">
         <ul className="menu">
           <li><a href="/">Home</a></li>
